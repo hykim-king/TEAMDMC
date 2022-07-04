c0b453e1ae2f2c9a90ef508b219969db40a3d7de
T2us <zzxsx484@gmail.com>
2022-07-04 13:40:17 +0900

    kemitController에서 balances 부분 제외함

--- src/main/java/com/teamdmc/kemie/controller/KemieController.java
+++ src/main/java/com/teamdmc/kemie/controller/KemieController.java
@@ -8,19 +8,6 @@ public class KemieController {
 	
 	public KemieController() {}
 	
-	@RequestMapping("/balancesPage.do")  
-	public String balancesPage() {
-		/*
-		 * HttpRequest request = HttpRequest.newBuilder()
-		 * .uri(URI.create("https://api.upbit.com/v1/market/all?isDetails=false"))
-		 * .header("Accept", "application/json") .method("GET",
-		 * HttpRequest.BodyPublishers.noBody()) .build(); HttpResponse<String> response
-		 * = HttpClient.newHttpClient().send(request,
-		 * HttpResponse.BodyHandlers.ofString()); System.out.println(response.body());
-		 */
-		return "balancesPage";
-	}
-	
 	@RequestMapping("/board.do")  
 	public String board() {
 		System.out.println("메인 테스트입니다 메인 테스트입니다.");
